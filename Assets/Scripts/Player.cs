70f3a26072bb9692c3ac378109f6b2a00c452250
Alex Gould <ag4536@nyu.edu>
2020-09-24 16:42:46 -0400

    basic init stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    	
+    }
+}
