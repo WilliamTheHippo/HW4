78f057fd334305cdbc2604220156c0d8d056ae7f
Alex Gould <ag4536@nyu.edu>
2020-09-24 17:08:15 -0400

    more init work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public float moveSpeed;
+	public float jumpSpeed;
+
 	Rigidbody2D rb;
+	float horizontal;
+	bool jumping;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        horizontal = 0f;
+        jumping = false;
     }
 
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+        	jumping = true;
+        }
     }
 
     void FixedUpdate()
     {
-    	
+    	rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    	if(jumping)
+    	{
+    		rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+    		jumping = false;
+    	}
     }
 }
