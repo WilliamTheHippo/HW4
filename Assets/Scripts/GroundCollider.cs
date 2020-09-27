7703ae2f864d7dc7da2e955d819c1477a29d5cb9
Alex Gould <ag4536@nyu.edu>
2020-09-26 20:06:56 -0400

    patches

---
+++ Assets/Scripts/GroundCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollider : MonoBehaviour
+{
+	public Player player;
+
+	void OnTriggerStay2D(Collider2D c)
+	{
+		if(c.tag == "Platform")
+		{
+			player.onGround = true;
+			player.canMoveInAir = true;
+		}
+		if(c.tag == "Debuff") player.canMoveInAir = false;
+		if(c.tag == "Bouncy") player.Bounce();
+		if(c.tag == "DebuffAndBouncy")
+		{
+			player.Bounce();
+			player.canMoveInAir = false;
+		}
+		if(c.tag == "Untagged") Debug.LogWarning("Untagged element!", c);
+	}
+
+	void OnTriggerExit2D(Collider2D c)
+	{
+		player.onGround = false;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,24 @@ public class Player : MonoBehaviour
 	public float moveSpeed;
 	public float jumpSpeed;
 	public bool onGround;
+	public bool canMoveInAir;
+	[Tooltip("Physics engine hackiness; higher value stops bouncing sooner to reduce jitter.")]
+	public float bounceThreshold;
 
 	Rigidbody2D rb;
 	float horizontal;
 	bool jumping;
+	bool bouncing;
+	float lastYVelocity;
+	float bounceVelocity;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         horizontal = 0f;
         jumping = false;
+        canMoveInAir = true;
+        bouncing = false;
     }
 
     void Update()
@@ -27,11 +35,30 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-    	rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    	lastYVelocity = rb.velocity.y;
+    	if(canMoveInAir || onGround)
+    	{
+    		rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    	}
     	if(jumping)
     	{
     		rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
     		jumping = false;
+    	}
+    	if(bouncing)
+    	{
+    		rb.velocity = new Vector2(rb.velocity.x, bounceVelocity);
+    		bouncing = false;
+    	}
+    }
+
+    public void Bounce()
+    {
+    	//Debug.Log(lastYVelocity);
+    	if(Mathf.Abs(lastYVelocity) > bounceThreshold)
+    	{
+    		bounceVelocity = -lastYVelocity;
+    		bouncing = true;
     	}
     }
 }
